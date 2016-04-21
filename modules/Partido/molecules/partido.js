72dce0214aee4bb25330edd5c27e91b1f55b6ceb
Suissa <jnascimento@gmail.com>
2016-04-21 02:38:16 -0300

    Molécula do Partido

---
+++ modules/Partido/molecules/partido.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Molecule = {
+  sigla: require('./../atoms/sigla')
+, nome: require('./../atoms/nome')
+, numero: require('./../atoms/numero')
+, presidente: require('./../atoms/presidente')
+, dataCriacao: require('./../atoms/dataCriacao')
+, dataRegistroDefinitivo: require('./../atoms/dataRegistroDefinitivo')
+, numeroAfiliados: require('./../atoms/numeroAfiliados')
+, espectroPolitico: require('./../atoms/espectroPolitico')
+, ideologia: require('./../atoms/ideologia')
+, site: require('./../atoms/site')
+, email: require('./../atoms/email')
+, telefoneCompleto: require('./../atoms/telefoneCompleto')
+, faxCompleto: require('./../atoms/faxCompleto')
+, enderecoPartido: require('./../atoms/enderecoPartido')
+, impeachment: require('./../atoms/impeachment')
+}
+
+module.exports = new Schema(Molecule);
--- modules/Partido/molecules/telefone.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const Molecule = {
-  ddd: require('./../atoms/phoneDDD')
-, numero: require('./../atoms/phoneNumber')
-}
-module.exports = new Schema(Molecule);
